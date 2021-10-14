8a2acdaf6b0b69ca25bf097863ea1b4f44aed4d1
Marcio Belloni <83551398+devbelloni@users.noreply.github.com>
2021-10-14 02:11:09 -0300

    Add files via upload

---
+++ exercicioproposto4/exercicioproposto4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercicioproposto4 {
+    class Program {
+        static void Main(string[] args) {
+            int n = int.Parse(Console.ReadLine());
+            int n1 = int.Parse(Console.ReadLine());
+            double sal = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double total = sal * n1;
+            Console.WriteLine($"NUMBER = {n}");
+            Console.WriteLine($"SALARY = U$ {total.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
---
+++ exercicioproposto5/exercicioproposto5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercicioproposto5 {
+    class Program {
+        static void Main(string[] args) {
+            string[] p1 = Console.ReadLine().Split(' ');
+            string[] p2 = Console.ReadLine().Split(' ');
+            string q1 = p1[1];
+            string v1 = p1[2];
+            string q2 = p2[1];
+            string v2 = p2[2];
+            int quant1 = int.Parse(q1);
+            double valor1 = double.Parse(v1, CultureInfo.InvariantCulture);
+            int quant2 = int.Parse(q2);
+            double valor2 = double.Parse(v2, CultureInfo.InvariantCulture);
+            double total = quant1 * valor1 + quant2 * valor2;
+            Console.WriteLine($"VALOR A PAGAR: R${total.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
